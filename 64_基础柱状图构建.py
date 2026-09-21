0f00ee7ffcc8723b8d05b1d733cee521dc713f30
落雨花 <yuhualuo043@gmail.com>
2026-09-21 11:19:31 +0800

    基础柱状图

--- 64_基础柱状图构建.py
+++ 64_基础柱状图构建.py
@@ -1,8 +1,13 @@
 from pyecharts.charts import Bar
+from pyecharts.options import LabelOpts
 
 bar = Bar()
 
-bar.add_xaxis(['中国', '美国', '英国'])
-bar.add_yaxis("GDP", [30, 20, 10])
+bar.add_xaxis(['中国', '美国', '英国', '韩国'])
+bar.add_yaxis("GDP", [30, 20, 10, 15], label_opts=LabelOpts(position="right"))     # 把数值标签放在右侧
+
+# 反转x、y轴
+bar.reversal_axis()
+
 
 bar.render("基础柱状图.html")
