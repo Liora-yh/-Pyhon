e563d2389f139b9b6937ba800ad160fad031a8fa
落雨花 <yuhualuo043@gmail.com>
2026-09-22 18:42:53 +0800

    封装

---
+++ 71_封装.py
@@ -0,0 +1,18 @@
+# 定义一个类，内涵私有成员变量和私有成员方法
+class Phone:
+
+    __current_voltage = 1    # 当前手机运行电压
+
+    def __keep_single_core(self):
+        print("让CPU以单核模式运行")
+
+    def call_by_5g(self):
+        if self.__current_voltage >= 1:
+            print("5g通话开启")
+        else:
+            self.__keep_single_core()
+            print("电量不足，无法使用5g通话，并设置为单核运行模式进行省电")
+
+phone = Phone()
+phone.call_by_5g()
+
